f94eba212ba3e50199d3ed33af684e1d1f5289f9
Marco Mangan <marco.mangan@pucrs.br>
2014-05-21 11:22:57 -0300

    #1

--- Vetores/main.c
+++ Vetores/main.c
@@ -1,8 +1,54 @@
+/*
+ marco.mangan@pucrs.br
+*/
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+#define MAX 3
+
+int main(void)
 {
-    printf("Hello world!\n");
+    printf("Vetor, (c) 2013.\n");
+
+    int v[MAX];
+    int i;
+
+    for(i = 0; i < MAX; i++) {
+        v[i] = 100+i;
+        /*scanf("%d", &v[i]);*/
+    }
+
+    for(i = 0; i < MAX; i++) {
+        printf("%d\n", v[i]);
+    }
+
+/* soma */
+    printf("\n");
+
+    int soma = 0;
+    for(i = 0; i < MAX; i++) {
+        soma += v[i];
+    }
+    printf("%d\n", soma);
+
+/* localizar valor */
+    printf("\n");
+    int x;
+    printf("Informe o valor: ");
+    scanf("%d", &x);
+    int pos = - 1;
+    for(i = 0; i < MAX; i++) {
+        if (v[i] == x)
+        {
+            pos = i;
+            break;
+        }
+    }
+    printf("%d\n", pos);
+
+
+
+    printf("*FIM*");
+
     return 0;
 }
+
